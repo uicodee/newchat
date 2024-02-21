3cc17a2f88c15085ba20aa3561bce07c9381b2f5
mac <uicodee@gmail.com>
2024-02-21 12:33:24 +0500

    upd

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,75 @@
 import {FC} from "react";
-import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 
 const Page: FC = () => {
     return (
         <div className="h-lvh flex justify-center items-center">
-            <Card className="max-w-72 md:max-w-md w-full">
-                <CardHeader>
-                    <CardTitle className="text-xl md:text-2xl">Kirish</CardTitle>
-                    <CardDescription>Akkauntingizga kiring</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="grid w-full items-center gap-4">
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" placeholder="example@mail.com" />
-                            </div>
-                            <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="password">Parol</Label>
-                                <Input type="password" id="password" placeholder="Password" />
-                            </div>
-                        </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex flex-col">
-                    <Button className="w-full">Kirish</Button>
-                </CardFooter>
-            </Card>
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="account">Войти</TabsTrigger>
+                    <TabsTrigger value="password">Зарегистрироваться</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                    <Card className="max-w-80 md:max-w-md w-full">
+                        <CardHeader>
+                            <CardTitle className="text-xl md:text-2xl">Войти</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input id="email" placeholder="example@mail.com" />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="password">Пароль</Label>
+                                        <Input type="password" id="password" placeholder="******" />
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button className="w-full">Войти</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="password">
+                    <Card className="max-w-80 md:max-w-md w-full">
+                        <CardHeader>
+                            <CardTitle className="text-xl md:text-2xl">Регистрация</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="email">Полное имя</Label>
+                                        <Input id="email" />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input id="email" placeholder="example@mail.com" />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        <Label htmlFor="password">Пароль</Label>
+                                        <Input type="password" id="password" placeholder="******" />
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button className="w-full">Регистрация</Button>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+
         </div>
     )
 }
+// internationalization => i18n
+// localization => l10n
 export default Page
